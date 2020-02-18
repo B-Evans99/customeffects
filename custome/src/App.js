ffb4998cf97a6676b805f89808623aec34a4167f
Bronwen Evans <T00586774@mytru.ca>
2020-02-18 14:14:32 -0800

    feat(card.js): init card and card list

--- custome/src/App.js
+++ custome/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Card from './components/card.js';
 import logo from './logo.svg';
 import './App.css';
 
 
 
 function App() {
+  let [effects,setEffects] = useState([1,2,3,4,5])
+
   return (
     <div className="App">
       <h1>hiiiiiii</h1>
+      {effects.map(effect=>{
+        return <Card></Card>
+      })}
     </div>
   );
 }
---
+++ custome/src/components/card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Card() {
+  return (
+    <div className="Card">
+      <h3>card</h3>
+    </div>
+  );
+}
+
+export default Card;
