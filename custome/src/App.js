fd3881688ff2a9c1ec899b8162b9d058c63b1cf6
B-Evans99 <4bronwenevans@gmail.com>
2020-02-19 23:28:59 -0800

    feat(app.js, app.css): add minor styling

--- custome/src/App.js
+++ custome/src/App.js
@@ -1,21 +1,20 @@
-import React, {useState} from 'react';
-import Card from './components/card.js';
-import Header from './components/header'
-import logo from './logo.svg';
-import './App.css';
-
-
+import React, { useState } from "react";
+import Card from "./components/card.js";
+import Header from "./components/header";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  let [effects,setEffects] = useState([1,2,3,4,5])
+  let [effects, setEffects] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
   return (
     <div className="App">
       <Header></Header>
-      <h1>hiiiiiii</h1>
-      {effects.map(effect=>{
-        return <Card></Card>
-      })}
+      <div className="cardBox">
+        {effects.map(effect => {
+          return <Card></Card>;
+        })}
+      </div>
     </div>
   );
 }
