6e50a1ae3d167cdd470294d3b228dc64d394c0aa
abittante <alliebittante@gmail.com>
2020-02-24 20:23:31 -0800

    (card.js, images, App.css): flesh out card design on initial page

--- custome/src/components/card.js
+++ custome/src/components/card.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import blur from "../images/blur.png";
+import star from "../images/star.png";
+import dot from "../images/dot.png";
 
+let ratingAverage = 4.8;
+let ratingTotal = 112;
+let downloadTotal = 299;
+let authorName = "author";
+let effectName = "blur effect";
 
 function Card() {
   return (
-    <div className="Card">
-      <h3>card</h3>
+    <div className="Card" >
+      <div className="cardAuthorName" style={{color:"#464646"}}>by {authorName}</div>
+      <img src= {blur} className="cardImg" alt="blur" />
+      <div style={{fontSize: "22px"}} className="cardEffectName">{effectName}</div>
+      <div className="cardText" style={{display:"flex", alignItems:"center"}}>
+        <img style={{paddingRight:"4px"}} src={star} className="starImg" alt="star" width={20}/>
+        <span className="cardRatingText">{ratingAverage} <span style={{color:"#464646"}} className="cardRatingTotal">({ratingTotal})</span></span>
+        <img style={{paddingTop: "4px", paddingRight: "6px", paddingLeft: "6px"}} src={dot} className="cardDotImg" alt="." width={5}/>
+        <span className="cardDownloadLine">{downloadTotal} <span style={{color:"#464646"}} className="cardDownloadText">downloads</span></span>
+      </div>
     </div>
   );
 }
