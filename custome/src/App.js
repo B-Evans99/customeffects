cef7fe0337e273b682e72a79dde1db17bd2e880f
abittante <alliebittante@gmail.com>
2020-02-25 16:50:46 -0800

    (categoryGroup, filterGroup, filter, category, app) change format of filters and categories, add click event to categories (console logs name right now)

--- custome/src/App.js
+++ custome/src/App.js
@@ -1,34 +1,23 @@
 import React, { useState } from "react";
 import Card from "./components/card.js";
-import Filter from "./components/filter.js";
+import FilterGroup from "./components/filterGroup.js";
 import Header from "./components/header.js";
-import Category from "./components/category.js";
+import CategoryGroup from "./components/categoryGroup.js";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   let [effects, setEffects] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+  let [filters, setFilters] = useState(["highest rating", "newest", "most downloads"]);
+  let [categories, setCategories] = useState(["blur", "colorization", "vintage", "rotational/skewing", "sharpening", "miscellaneous"]);
 
   return (
     <div className="App" style={{display:"flex", flexDirection: "column"}}>
       <Header/>
       <div style={{display: "flex", flexDirection:"row"}}>
       <div className="filterBox" >
-        <div>
-          filters:
-          <Filter/>
-          <Filter/>
-          <Filter/>
-          categories:
-          <Category/>
-          <Category/>
-          <Category/>
-          <Category/>
-          <Category/>
-          <Category/>
-          <Category/>
-          <Category/>
-        </div>
+          <FilterGroup filters = {filters}/>
+          <CategoryGroup categories = {categories} />
       </div>
       <div className="cardBox">
         {effects.map(effect => {
