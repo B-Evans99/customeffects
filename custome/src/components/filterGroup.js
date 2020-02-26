4f59bef4290c0cab2ced3e6b134a83918a856fc6
abittante <alliebittante@gmail.com>
2020-02-26 13:38:05 -0800

    (App.js, filter.js, filtergroup.js) add checkbox back to filters, only one can be checked now, default is rating

--- custome/src/components/filterGroup.js
+++ custome/src/components/filterGroup.js
@@ -5,26 +5,35 @@ import "../App.css";
 const FilterGroup = props => {
   let filters = props.filters;
 
+  let [checked, setChecked] = useState(filters[0]);
+
+
   return (
     <div className="FilterGroup">
       <span style={{ fontSize: "20px" }}>sort by:</span>
       <Filter
         effects={props.effects}
         setEffects={props.setEffects}
-        filter={"rating"}
+        filter={filters[0]}
         modify={props.sortByRating}
+        checked= {checked}
+        setChecked = {setChecked}
       />
       <Filter
         effects={props.effects}
         setEffects={props.setEffects}
-        filter={"downloads"}
+        filter={filters[1]}
         modify={props.sortByDownloads}
+        checked= {checked}
+        setChecked = {setChecked}
       />
       <Filter
         effects={props.effects}
         setEffects={props.setEffects}
-        filter={"newest"}
+        filter={filters[2]}
         modify={props.sortByNewest}
+        checked= {checked}
+        setChecked = {setChecked}
       />
     </div>
   );
