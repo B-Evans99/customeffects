f47fb94f9d4dd2a6a28cb6eed4611282f6abe9e2
abittante <alliebittante@gmail.com>
2020-02-25 17:46:07 -0800

    (App.css, header.js, profile.png) add profile button, change header layout, add click events to logo and account buttons that console log

--- custome/src/components/header.js
+++ custome/src/components/header.js
@@ -1,10 +1,42 @@
 import React from "react";
+import "../App.css";
+import profile from "../images/profile.png";
 import SearchBar from "./searchbar";
 
-function Header() {
+const Header = () =>{
+
+  const logoClick = () =>{
+    console.log("Logo Click");
+  }
+
+  const registerClick = () => {
+    console.log("Register Click");
+  }
+
+  const loginClick = () => {
+    console.log("Login Click");
+  }
+
+  const profileClick = () => {
+    console.log("Profile Click");
+  }
+
   return (
     <div className="Header">
-      logo <SearchBar></SearchBar> register login
+      <div className = "Logo" onClick={logoClick}>
+        logo 
+      </div>
+      <SearchBar /> 
+      <div className = "accountButtons">
+        <img src = {profile} alt="profile" onClick = {profileClick} className = "profileIcon" />
+        <div className = "registerButton" onClick= {registerClick}>
+          register
+        </div> 
+        <div className = "loginButton" onClick={loginClick}>
+          login
+        </div>
+      </div>
+      
     </div>
   );
 }
