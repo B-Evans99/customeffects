b1dd23da10ed539748250a7d6887f91111922d21
B-Evans99 <4bronwenevans@gmail.com>
2020-02-28 00:31:48 -0800

    feat(App.css, resultCard.js): add styling to the results page

--- custome/src/components/resultCard.js
+++ custome/src/components/resultCard.js
@@ -20,38 +20,52 @@ const ResultCard = props => {
   };
 
   const resultClick = () => {
-    setFocusedEffect(prevstate => prevstate = effect);
-  }
+    setFocusedEffect(prevstate => (prevstate = effect));
+  };
 
   return (
-    <div className="resultCard" onClick={resultClick} style={focusedEffect == effect? focusStyle: {}}>
-        <img src={resultImg} className="resultCardImg" alt="blur" height={90} />
-        <div className="resultCardText">
-            <span className="cardEffectName">{effect.name}</span>
-                <div className="resultRateDownloadText">
-                    <img
-                        src={star}
-                        style={{ paddingRight: "4px" }}
-                        className="cardStarImg"
-                        alt="star"
-                        width={20}
-                    />
-                    <span className="cardRatingText">
-                        {effect.rating}{" "}
-                        <span className="cardRatingTotal">({effect.reviewers})</span>
-                    </span>
-                    <img src={dot} className="cardDotImg" alt="." width={5} />
-                    <span className="cardDownloadLine">
-                        {effect.downloads} <span className="cardDownloadText">downloads</span>
-                    </span>
-                </div>
-                <div className="cardAuthorName">by {users.users[effect.user].name}
-                    <img style={{paddingBottom: "2px"}} src={dot} className="cardDotImg" alt="." width={3} />
-                    <span className="date">
-                        {new Date(effect.date * 1000).getFullYear()}
-                    </span>
-                </div>
+    <div
+      className="resultCard"
+      onClick={resultClick}
+      style={focusedEffect == effect ? focusStyle : {}}
+    >
+      <div className="resultCardImg">
+        <img src={resultImg} alt={effect.name} height={90} />
+      </div>
+      <div className="resultCardText">
+        <span className="cardEffectName">{effect.name}</span>
+        <div className="resultRateDownloadText">
+          <img
+            src={star}
+            style={{ paddingRight: "4px" }}
+            className="cardStarImg"
+            alt="star"
+            width={20}
+          />
+          <span className="cardRatingText">
+            {effect.rating}{" "}
+            <span className="cardRatingTotal">({effect.reviewers})</span>
+          </span>
+          <img src={dot} className="cardDotImg" alt="." width={5} />
+          <span className="cardDownloadLine">
+            {effect.downloads}{" "}
+            <span className="cardDownloadText">downloads</span>
+          </span>
         </div>
+        <div className="cardAuthorName">
+          by {users.users[effect.user].name}
+          <img
+            style={{ paddingBottom: "2px" }}
+            src={dot}
+            className="cardDotImg"
+            alt="."
+            width={3}
+          />
+          <span className="date">
+            {new Date(effect.date * 1000).getFullYear()}
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
