67f4b0ac8c9b046491a139a04f654c7db3f75bb7
abittante <alliebittante@gmail.com>
2020-03-02 13:20:56 -0800

    (download.png, descriptionBox, app.css) add download button to decription box in results page, it currently only fires an onclick

--- custome/src/components/descriptionBox.js
+++ custome/src/components/descriptionBox.js
@@ -2,6 +2,7 @@ import React from "react";
 import resultImg from "../images/result.png";
 import resultImg2 from "../images/result_big.png";
 import dot from "../images/dot.png";
+import download from "../images/download.png";
 import "../App.css";
 import users from "../data/users.js";
 
@@ -11,15 +12,24 @@ const DescriptionBox = props => {
   let setResultsType = props.setResultsType;
   let effect = props.focusedEffect;
 
+  const downloadClick = () =>{
+    console.log("download click");
+  }
+
   return (
     <div className="descriptionBox">
       <img src={resultImg2} alt="effect image" className="descriptionImage" />
       <div className="descriptionText" >
+      <div className="descriptionHeader">
+        <div className="decriptionHeaderLeft">
         <div className="descriptionName">{effect.name}</div>
-        <div className="descriptionAuthYear">
-          <span className="descriptionAuth">by {users.users[effect.user].name}</span>
-          <img style={{paddingBottom: "2px"}} src={dot} className="cardDotImg" alt="." width={3} />
-          <span className="descriptionYear">{new Date(effect.date * 1000).getFullYear()}</span>
+            <div className="descriptionAuthYear">
+              <span className="descriptionAuth">by {users.users[effect.user].name}</span>
+              <img style={{paddingBottom: "2px"}} src={dot} className="cardDotImg" alt="." width={3} />
+              <span className="descriptionYear">{new Date(effect.date * 1000).getFullYear()}</span>
+            </div>
+          </div>
+          <img className="downloadButton" src = {download} alt="download" onClick={downloadClick} />
         </div>
         <div className="descriptionDesc">{effect.desc}</div>
       </div> 
