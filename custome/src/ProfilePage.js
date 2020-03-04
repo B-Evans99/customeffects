d7a991fcacbaf10c966f5d8618be1af9a2e01cd7
abittante <alliebittante@gmail.com>
2020-03-04 14:26:10 -0800

    (Profile Page) add comments related to dropzone

--- custome/src/ProfilePage.js
+++ custome/src/ProfilePage.js
@@ -10,6 +10,8 @@ const ProfilePage = (props) => {
     return (
         <div className = "ProfilePage">
             Profile Page
+            {/* This may need to live in the form - idk 
+                here's the link to the website: https://github.com/react-dropzone/react-dropzone */}
             <Dropzone/>
             <UploadForm />
         </div>
